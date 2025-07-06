15e4daed6b67cc4091a074db158cd4bcc5688d58
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2025-07-07 00:59:05 +0700

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:4200",
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`>>> Server is running on port: ${port}`);

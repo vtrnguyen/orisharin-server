37cf21516fb95c8ac6b5ce6569785dc8dc30a31f
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2025-08-02 10:41:27 +0700

    changed api response dto

--- src/common/dtos/api-response.dto.ts
+++ src/common/dtos/api-response.dto.ts
@@ -3,10 +3,12 @@ export class ApiResponseDto<T> {
     success: boolean;
     message: string;
     data: T;
+    error: string | null;
 
-    constructor(data: T, message = "", success = true) {
+    constructor(data: T, message = "", success = true, error: string | null = null) {
         this.data = data;
         this.message = message;
         this.success = success;
+        this.error = error;
     }
 }
